036dda3472dc395135527470b64072bcbf5d5b8d
e-lamine <lamine@e-lamine.dev>
2023-04-29 18:36:22 +0100

    pubSub module

---
+++ src/pubSub.js
@@ -0,0 +1,51 @@
+const pubsub = () => {
+   const events = {};
+   let subscriptionsId = -1;
+
+   function publish(event, data) {
+      if (!events[event]) {
+         return false;
+      }
+
+      events[event].forEach((subscription) => {
+         subscription.func(data);
+      });
+      return true;
+   }
+
+   function subscribe(event, func) {
+      if (!events[event]) {
+         events[event] = [];
+      }
+
+      subscriptionsId += 1;
+      const token = subscriptionsId.toString();
+      events[event].push({
+         token,
+         func,
+      });
+      return token;
+   }
+
+   function unsubscribe(token) {
+      const found = Object.keys(events).some((event) =>
+         events[event].some((subscription, index) => {
+            const areEqual = subscription.token === token.toString();
+            if (areEqual) {
+               events[event].splice(index, 1);
+            }
+            return areEqual;
+         })
+      );
+
+      return found ? token : null;
+   }
+
+   return {
+      publish,
+      subscribe,
+      unsubscribe,
+   };
+};
+
+export default pubsub;
