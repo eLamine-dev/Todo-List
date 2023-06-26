dc34473aec04e686349d7d62ac6eef43e3e5476b
e-lamine <lamine@e-lamine.dev>
2023-06-26 17:31:14 +0100

    adding select project and select priority to tasks form

--- src/views/components/AddTaskForm.js
+++ src/views/components/AddTaskForm.js
@@ -13,15 +13,19 @@ class AddTaskForm extends HTMLFormElement {
    }
 
    render() {
-      const titleInput = createElement('input').setAttributes({
-         type: 'text',
-         name: 'title-input',
-      });
-      const dateInput = createElement('input').setAttributes({
-         type: 'date',
-         name: 'date-input',
-         // min: '',
-      });
+      const titleInput = createElement('input')
+         .setAttributes({
+            type: 'text',
+            name: 'title-input',
+         })
+         .appendTo(this);
+      const dateInput = createElement('input')
+         .setAttributes({
+            type: 'date',
+            name: 'date-input',
+            // min: '',
+         })
+         .appendTo(this);
       dateInput.valueAsDate = new Date();
 
       const submitBtn = createElement('button')
@@ -29,21 +33,23 @@ class AddTaskForm extends HTMLFormElement {
             type: 'submit',
             name: 'save-task',
          })
-         .setContent('save');
+         .setContent('save')
+         .appendTo(this);
 
-      const selectProject = createElement('select').setAttributes({
-         class: 'select-project',
-      });
+      this.setupSelectProjectList();
 
-      this.setupSelectList(selectProject);
+      this.setUpPriorities();
 
       this.id = 'new-task-form';
-      [titleInput, dateInput, submitBtn, selectProject].forEach((child) => {
-         this.appendChild(child);
-      });
    }
 
-   setupSelectList(selectList) {
+   setupSelectProjectList() {
+      const selectProject = createElement('select')
+         .setAttributes({
+            class: 'select-project',
+            name: 'select-project',
+         })
+         .appendTo(this);
       this.categorizedProjects.forEach((category) => {
          const optGrp = createElement('optgroup').setAttributes({
             label: category.category.title,
@@ -51,12 +57,30 @@ class AddTaskForm extends HTMLFormElement {
          category.categoryProjects.forEach((project) => {
             const option = createElement('option')
                .setAttributes({
-                  value: project.title,
+                  id: project.id,
                })
                .setContent(project.title);
             optGrp.appendChild(option);
          });
-         selectList.appendChild(optGrp);
+         selectProject.appendChild(optGrp);
+      });
+   }
+
+   setUpPriorities() {
+      const selectPriority = createElement('select')
+         .setAttributes({
+            class: 'select-priority',
+            name: 'select-priority',
+         })
+         .appendTo(this);
+      const priorities = [1, 2, 3, 4];
+      priorities.forEach((priority) => {
+         const option = createElement('option')
+            .setAttributes({
+               id: `${priority}`,
+            })
+            .setContent(`Priority ${priority}`);
+         selectPriority.appendChild(option);
       });
    }
 
@@ -68,12 +92,17 @@ class AddTaskForm extends HTMLFormElement {
    }
 
    passData() {
+      const selectProject = this.elements['select-project'];
+      const selectPriority = this.elements['select-priority'];
       const formData = {
          dataType: 'task',
          title: this.elements['title-input'].value,
          date: this.elements['date-input'].value,
+         projectId: selectProject.options[selectProject.selectedIndex].id,
+         priority: selectPriority.options[selectPriority.selectedIndex].id,
       };
       pubsub.publish('task:add', formData);
+      console.log(formData);
    }
 }
 customElements.define('add-task-form', AddTaskForm, { extends: 'form' });
